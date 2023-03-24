3ab9beb5d975cccaeaf1cec7fbda260b40da6007
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-03-24 10:23:22 +0700

    Add project files.

---
+++ insersensort_129/insersensort_129.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "masukan jumlah data array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukan maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "masukan element array" << endl;
+	cout << "=====================" << endl;
+
+}
